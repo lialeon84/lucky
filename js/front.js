65b6a7900623ea4a494bcc6dad7505933b9e180f
Rose Vergel <50724307+lialeon84@users.noreply.github.com>
2021-05-16 19:18:41 -0500

    added a visitor counter

--- js/front.js
+++ js/front.js
@@ -496,3 +496,15 @@ function mySeven() {
     };
     $("#genseven5").appendVal(numRnn);
 }
+
+var n = localStorage.getItem("on_load_counter");
+
+if (n === null) {
+    n = 0;
+}
+
+n++;
+
+localStorage.setItem("on_load_counter", n);
+
+document.getElementById("CounterVisitor").innerHTML = n;
